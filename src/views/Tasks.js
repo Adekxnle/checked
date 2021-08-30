58af2efe343052dd9366cdc3559eb5c412d029a6
Adekunle Ashaolu <dekxnle@gmail.com>
2021-08-30 11:05:43 +0100

    working on tasks page and components

--- src/views/Tasks.js
+++ src/views/Tasks.js
@@ -1,9 +1,15 @@
-import NoTask from './NoTask';
+//import NoTask from './NoTask';
 
 function Tasks() {
   return (
-    <div>
-      <NoTask />
+    <div className='tasks-page'>
+      <div className='task-container'>
+        <input type='checkbox' />
+        <div className='task'>
+          <div className='indicator' />
+          <p>Cut the crap!!</p>
+        </div>
+      </div>
     </div>
   );
 }
