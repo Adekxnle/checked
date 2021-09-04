7deae9104258ab6611b1808e657647de714ddf9f
Adekunle Ashaolu <dekxnle@gmail.com>
2021-09-04 23:01:15 +0100

    some file rearragements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import './App.css';
-import Header from './components/Header';
+import calenderIcon from './assets/calender.svg';
+import plusIcon from './assets/plus.svg';
 import Tasks from './views/Tasks';
 import CompletedTasks from './views/CompletedTasks';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link, useLocation } from 'react-router-dom';
 
 function App() {
+  let location = useLocation();
+
   return (
     <div className='app'>
-      <Header />
+      <div className='header'>
+        <div className='header-top'>
+          <div className='header-top-left'>
+            <span className='header-bullet' />
+            <div className='date-container'>
+              <h1 className='date'>August 21, 2021</h1>
+              <img src={calenderIcon} alt='calender icon' />
+            </div>
+          </div>
+          <button className='add-new-task-btn'>
+            <img src={plusIcon} alt='plus icon' />
+            <span>add new task</span>
+          </button>
+        </div>
+        <div className='header-bottom'>
+          <ul>
+            <li>
+              <Link className='tabs' to='/'>
+                <div className='tab'>
+                  <h2> Tasks </h2>
+                  {location.pathname === '/' ? <div className='active' /> : ''}
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link className='tabs' to='/completed-tasks'>
+                <div className='tab'>
+                  <h2>Completed Tasks</h2>
+                  {location.pathname === '/completed-tasks' ? <div className='active' /> : ''}
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className='pages'>
         <Switch>
           <Route exact path='/'>
@@ -18,6 +55,7 @@ function App() {
           </Route>
         </Switch>
       </div>
+      <div className='add-new'></div>
     </div>
   );
 }
---
+++ src/views/NewTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewTask() {
+  return (
+    <div className='new-task-page'>
+      <h1>new task</h1>
+    </div>
+  );
+}
+
+export default NewTask;
