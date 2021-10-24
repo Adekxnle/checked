8e74741045548a429d4bf6e0f9c920f622910750
Adekunle Ashaolu <dekxnle@gmail.com>
2021-10-24 22:23:17 +0100

    new component changes

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const TaskContext = createContext();
 
 function TaskContextProvider(props) {
-  const [tasks, setTasks] = useState([
+  const [tasks] = useState([
     { id: 1, title: 'wash them plates!', badge: '#7f5af0' },
     { id: 2, title: 'feed the dogs!', badge: '#ff8906' },
   ]);
