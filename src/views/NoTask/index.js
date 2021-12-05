d67d230478fb0a9c200d0608d04cee3da5dba3a5
Adekunle Ashaolu <dekxnle@gmail.com>
2021-12-05 17:22:25 +0100

    finished responsiveness on no-task page illustration

--- src/views/NoTask/index.js
+++ src/views/NoTask/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import illustration_200 from '../../assets/illustration_w-200.svg';
 import illustration_300 from '../../assets/illustration_w-300.svg';
+import illustration_400 from '../../assets/illustration_w-400.svg';
 import illustration_500 from '../../assets/illustration_w-500.svg';
 
 function NoTask() {
@@ -8,7 +9,7 @@ function NoTask() {
     <div className='pages no-task-page'>
       <img
         src={illustration_300}
-        srcSet={`${illustration_200} 200w, ${illustration_300} 600w, ${illustration_500} 1200w`}
+        srcSet={`${illustration_200} 200w, ${illustration_300} 400w, ${illustration_300} 800w, ${illustration_400} 1100w, ${illustration_500} 1900w`}
         alt='illustration of a person relaxing'
       />
       <p>You don't have any task set for today</p>
