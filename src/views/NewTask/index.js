dd3a2f361e65fc4d0085e4962b7206d9809ba130
Adekunle Ashaolu <dekxnle@gmail.com>
2022-01-12 12:45:42 +0100

    working on add new task page

--- src/views/NewTask/index.js
+++ src/views/NewTask/index.js
@@ -12,11 +12,15 @@ function NewTask() {
   const [title, setTitle] = useState('');
   const [badge, setBadge] = useState('');
 
-  const handleAddNewTask = () => {
+  const handleAddNewTask = (e) => {
+    e.preventDefault();
+
     const newTask = { title, badge };
     if (newTask.title !== '' && newTask.badge !== '') {
       addNewTask(newTask);
     }
+
+    setTitle('');
   };
 
   const handleSetBadge = (e) => (e.target.checked ? setBadge(e.target.value) : '');
@@ -32,77 +36,79 @@ function NewTask() {
   return (
     <div className='new-task-page-wrapper' style={{ display: isOpen ? 'grid' : 'none' }}>
       <div className='new-task-page'>
-        <div className='new-task-header'>
-          <div className='new-task-header-left'>
+        <form onSubmit={(e) => handleAddNewTask(e)}>
+          <div className='new-task-header'>
+            <div className='new-task-header-left'>
+              <svg
+                onClick={toggleIsOpen}
+                width='14'
+                height='14'
+                viewBox='0 0 14 14'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z'
+                  fill='#e6e6e6'
+                />
+              </svg>
+              <h1>New task</h1>
+            </div>
+            <button type='submit' className='btn-finished'>
+              <img src={Checked} alt='checked icon' />
+              <span>done</span>
+            </button>
+          </div>
+          <div className='new-task-page-content'>
+            <div className='input-title'>
+              <label htmlFor='title'>Title</label>
+              <textarea
+                value={title}
+                type='text'
+                id='title'
+                maxLength='150'
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className='color-badge'>
+              <p>Color badge</p>
+              <div className='colors'>
+                <ul>
+                  {colours.map((colour) => {
+                    return (
+                      <li key={colour.id}>
+                        <label className='badge'>
+                          <span className='badge-input'>
+                            <input
+                              id={colour.id}
+                              value={colour.colour}
+                              type='radio'
+                              name='badge'
+                              onChange={(e) => handleSetBadge(e)}
+                              style={{ backgroundColor: colour.colour }}
+                            />
+                          </span>
+                        </label>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <a href='/#' className='btn-finished-sm'>
             <svg
-              onClick={toggleIsOpen}
-              width='14'
+              width='18'
               height='14'
-              viewBox='0 0 14 14'
+              viewBox='0 0 18 14'
               fill='none'
               xmlns='http://www.w3.org/2000/svg'>
               <path
-                d='M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z'
+                d='M5.6 10.6L1.4 6.40002L0 7.80002L5.6 13.4L17.6 1.4L16.2 0L5.6 10.6Z'
                 fill='#e6e6e6'
               />
             </svg>
-            <h1>New task</h1>
-          </div>
-          <button className='btn-finished' onClick={handleAddNewTask}>
-            <img src={Checked} alt='checked icon' />
-            <span>done</span>
-          </button>
-        </div>
-        <div className='new-task-page-content'>
-          <div className='input-title'>
-            <label htmlFor='title'>Title</label>
-            <textarea
-              value={title}
-              type='text'
-              id='title'
-              maxLength='150'
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className='color-badge'>
-            <p>Color badge</p>
-            <div className='colors'>
-              <ul>
-                {colours.map((colour) => {
-                  return (
-                    <li key={colour.id}>
-                      <label className='badge'>
-                        <span className='badge-input'>
-                          <input
-                            id={colour.id}
-                            value={colour.colour}
-                            type='radio'
-                            name='badge'
-                            onChange={(e) => handleSetBadge(e)}
-                            style={{ backgroundColor: colour.colour }}
-                          />
-                        </span>
-                      </label>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <a href='/#' className='btn-finished-sm'>
-          <svg
-            width='18'
-            height='14'
-            viewBox='0 0 18 14'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'>
-            <path
-              d='M5.6 10.6L1.4 6.40002L0 7.80002L5.6 13.4L17.6 1.4L16.2 0L5.6 10.6Z'
-              fill='#e6e6e6'
-            />
-          </svg>
-        </a>
+          </a>
+        </form>
         {/* <div className='dummy-div'></div> */}
       </div>
     </div>
